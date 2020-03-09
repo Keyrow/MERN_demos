b487f2ab5027e51b3e133db172bd40f4cfdfb5cf
Neil Mosunic <neilm813@gmail.com>
2020-03-09 12:10:40 -0700

    re-org

---
+++ w2/d1/todos-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <div className="container-flex">
+      <Todos></Todos>
+    </div>
+  );
+}
+
+export default App;
