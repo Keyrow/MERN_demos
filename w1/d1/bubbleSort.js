cce9db5aa0f09d8822b3b078fd3c7c1bc785b9e3
Neil M <neilm813@gmail.com>
2020-05-04 08:58:50 -0700

    algos

---
+++ w1/d1/bubbleSort.js
@@ -0,0 +1,14 @@
+/* 
+  http://algorithms.dojo.news/static/Algorithms/index.html#LinkTarget_2140
+
+  Array: Bubble Sort
+
+  Time Complexity
+    - Best: n when array is already sorted
+    - Average: O(n^2)
+    - Worst: O(n^2) when the array is reverse sorted
+
+  For review, create a function that uses BubbleSort to sort an unsorted array in-place.
+
+  For every pair of adjacent indicies, swap them if the item on the left is larger than the item on the right until array is fully sorted
+*/
