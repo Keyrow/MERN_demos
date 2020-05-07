fab5564327ba87bc77ac327b1d28b56a53a76a00
Neil M <neilm813@gmail.com>
2020-05-07 09:17:01 -0700

    partition slns

--- w1/d4/partition.js
+++ w1/d4/partition.js
@@ -18,3 +18,57 @@
 
   3. return: new idx of the pivot value
 */
+
+// Hoare’s partitioning scheme, named for Sir Charles Antony Richard Hoare, who developed the quicksort algorithm.
+// does fewer swaps than many other techniques
+
+//    [11, 8, 14, 3, 6, 2, 7]
+// => [2, 8, 14, 3, 6, 11, 7] 11 & 2 swapped
+// => [2, 3, 14, 8, 6, 11, 7] 3 & 8 swapped
+
+function partitionHoare(arr, left, right) {
+  const pivot = arr[Math.floor((left + right) / 2)];
+
+  while (left <= right) {
+    while (arr[left] < pivot && left <= right) {
+      left++;
+    }
+    while (arr[right] > pivot) {
+      right--;
+    }
+    if (left <= right) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+  }
+  return left;
+}
+// destructure syntax ex: arr = [1, 2, 3]
+// let [first, second] = [arr[0], arr[1]];
+// first & second are now variables equal to first and second items of arr respectively
+
+// Lomuto partition scheme, it is less efficient than the Hoare partition scheme
+//    [11, 8, 14, 3, 6, 2, 7]
+// => [3, 8, 14, 11, 6, 2, 7] 11 & 3 swapped
+// => [3, 6, 14, 11, 8, 2, 7] 8 & 6 swapped
+// => [3, 6, 2, 11, 8, 14, 7] 2 & 14 swapped
+// => [3, 6, 2, 7, 8, 14, 11] 7 & 11 swapped
+
+function partitionLomuto(nums, low, high) {
+  const pivot = nums[high];
+  let i = low;
+
+  for (let j = low; j < high; j++) {
+    if (nums[j] <= pivot) {
+      // swap nums at i and j
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      i++;
+    }
+  }
+  // final swap
+  [nums[i], nums[high]] = [nums[high], nums[i]];
+  return i;
+}
+
+const a = [11, 8, 14, 3, 6, 2, 7];
