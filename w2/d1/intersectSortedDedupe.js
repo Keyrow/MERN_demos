64d23b5f2b81dab38f07525fa9a618dd84766a40
Neil M <neilm813@gmail.com>
2020-05-11 08:51:57 -0700

    rename & algo reqs

---
+++ w2/d1/intersectSortedDedupe.js
@@ -0,0 +1,11 @@
+/* 
+  Intersect Sorted Arrays (dedupe)
+
+  Efficiently combine two already sorted multiset arrays 
+  into an array containing the sorted set intersection of the two.
+
+  Output: only the shared values between the two arrays (deduped).
+  
+  Input: [1,2,2,2,7], [2,2,6,6,7]
+  Output: [2,7]
+*/
