61af90e3c530f6548b83d936489f1fd38b69d2a2
Neil M <neilm813@gmail.com>
2020-05-27 10:29:51 -0700

    algo slns

--- w4/3_Wed/canBuildS1FromS2.js
+++ w4/3_Wed/canBuildS1FromS2.js
@@ -7,14 +7,52 @@
   .indexOf will only tell you if the letter is found one time
 */
 
-const input1StrA = "Hello World";
-const input1StrB = "lloeh wordl";
+// Inputs
+const StrA1 = "Hello World";
+const StrB1 = "lloeh wordl";
 // Output: true
 
-const input2StrA = "Hey";
-const input2StrB = "hello";
+// Inputs
+const StrA2 = "Hey";
+const StrB2 = "hello";
 // Output: false, second string is missing a "y"
 
-const input3StrA = "hello";
-const input3StrB = "helo";
+// Inputs
+const StrA3 = "hello";
+const StrB3 = "helo";
 // Output: false, second string doesn't have enough "l" letters
+
+// Inputs
+const StrA4 = "hello";
+const StrB4 = "lllheo";
+// Output: true
+
+// Time: O(n + m) -> O(n) linear, n = neededStr.length, m = availableStr.length
+// Space: O(m) -> O(n)
+function canBuildS1FromS2(neededStr, availableStr) {
+  const availableCharsFreq = {};
+
+  for (const availableChar of availableStr) {
+    const availableCharLower = availableChar.toLowerCase();
+
+    if (availableCharsFreq.hasOwnProperty(availableCharLower) === false) {
+      availableCharsFreq[availableCharLower] = 1;
+    } else {
+      availableCharsFreq[availableCharLower]++;
+    }
+  }
+
+  for (const neededChar of neededStr) {
+    const neededCharLower = neededChar.toLowerCase();
+
+    if (availableCharsFreq.hasOwnProperty(neededCharLower) === false) {
+      return false;
+    } else if (availableCharsFreq[neededCharLower] === 0) {
+      // not enough of this char available
+      return false;
+    } else {
+      availableCharsFreq[neededCharLower]--;
+    }
+  }
+  return true;
+}
