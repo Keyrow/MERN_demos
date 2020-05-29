3851f992ea6faec0fef6064854ef3a1e616185e0
Neil M <neilm813@gmail.com>
2020-05-29 08:59:31 -0700

    algo reqs

---
+++ w4/5_Fri/lengthOfLongestSubstring.js
@@ -0,0 +1,20 @@
+/* 
+  Given a string, find the length of the longest substring without repeating characters.
+*/
+
+const test1 = "abcabcbb";
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+const test2 = "bbbbb";
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+const test3 = "pwwkew";
+// Output: 3
+/* Explanation: The answer is "wke", with the length of 3. 
+  Note that the answer must be a substring, "pwke" is a subsequence and not a substring. */
+
+const test4 = "dvadf";
+// Output: 4
+// Explanation: "vadf"
