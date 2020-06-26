0fd7ca7723d0aaf774efa30843bebd98741bbb07
Neil M <neilm813@gmail.com>
2020-06-26 16:37:35 -0700

    blank slate

--- cities-demo/client/src/views/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NotFound = (props) => {
-  console.log(props);
-
-  return <div>Page not found: {props.location.href}</div>;
-};
-
-export default NotFound;
--- w2/useffect-and-routing/src/views/Secondary.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Secondary = (props) => {
-  return <div>Secondary</div>;
-};
-
-export default Secondary;
