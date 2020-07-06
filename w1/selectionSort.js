899bac17fd7789d8be8ce5330d1b668d07dcd202
Neil M <neilm813@gmail.com>
2020-07-06 10:10:12 -0700

    algo slns

--- w1/selectionSort.js
+++ w1/selectionSort.js
@@ -14,3 +14,27 @@
   - ideal for: pagination, where page 1 displays 10 records for example, you run selection sort for 10 iterations only to display the first 10 sorted items
 
 */
+
+function selectionSort(nums) {
+  const len = nums.length;
+  let selectedIdx = 0;
+  let idxOfCurrMin = 0;
+
+  while (selectedIdx < len) {
+    for (let i = selectedIdx; i < len; i++) {
+      if (nums[i] < nums[idxOfCurrMin]) {
+        idxOfCurrMin = i;
+      }
+    }
+
+    if (nums[selectedIdx] !== nums[idxOfCurrMin]) {
+      const temp = nums[selectedIdx];
+      nums[selectedIdx] = nums[idxOfCurrMin];
+      nums[idxOfCurrMin] = temp;
+    }
+    selectedIdx += 1;
+    // reset idxOfCurrMin to the next selected index we are going to work with to find the next min
+    idxOfCurrMin = selectedIdx;
+  }
+  return nums;
+}
