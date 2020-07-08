b5c1f9376a16d7ca415267eef99ddf2b04e11419
Neil M <neilm813@gmail.com>
2020-07-08 10:12:45 -0700

    algo sln

--- w1/d3/mergeSort.js
+++ w1/d3/mergeSort.js
@@ -1,4 +1,5 @@
 /* 
+  http://algorithms.dojo.news/static/Algorithms/index.html#LinkTarget_2140
 
   Stable
 
@@ -24,3 +25,54 @@ const mergeArrX = [3, 5, 7];
 const mergeArrY = [1, 2, 9, 10, 15];
 
 const arrRandom = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+
+// sorts two already separately sorted arrays
+function merge(left, right) {
+  let result = [];
+  let indexLeft = 0;
+  let indexRight = 0;
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] < right[indexRight]) {
+      result.push(left[indexLeft]);
+      indexLeft++;
+    } else {
+      result.push(right[indexRight]);
+      indexRight++;
+    }
+  }
+
+  // in case one of the arrays has remaining items due to unequal lengths, all of those can be added
+  while (indexLeft < left.length) {
+    result.push(left[indexLeft]);
+    indexLeft++;
+  }
+
+  while (indexRight < right.length) {
+    result.push(right[indexRight]);
+    indexRight++;
+  }
+
+  return result;
+
+  // one liner version of adding in any left over items
+  // return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+}
+
+// Split the array into halves and merge them recursively
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    // return once we hit an array with a single item
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2); // get the middle item of the array rounded down
+  const left = arr.slice(0, middle); // items on the left side
+  const right = arr.slice(middle); // items on the right side
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+const list = [2, 5, 1, 3, 7, 2, 3, 8, 6, 3];
+console.log(mergeSort(list).join(", ")); // [ 1, 2, 2, 3, 3, 3, 5, 6, 7, 8 ]
+// console.log(merge([1, 4, 6, 9], [1, 2, 3]).join(", "));
