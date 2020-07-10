99c5f0ad007edac18b05de8752fda263c004bff0
Neil M <neilm813@gmail.com>
2020-07-10 14:11:03 -0700

    initial setup

---
+++ w1/d5/tinder/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+// prop-it-up & putting-it-together demo but slightly different
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
