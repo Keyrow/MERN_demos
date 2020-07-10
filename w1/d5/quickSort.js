3d7ebf0c633ec242c9ef1c251569a901737c7eaa
Neil M <neilm813@gmail.com>
2020-07-10 10:06:04 -0700

    quickSort

--- w1/d5/quickSort.js
+++ w1/d5/quickSort.js
@@ -20,3 +20,17 @@
     - start by partitioning the full array (use the previously built partition algo)
     - then partition the left side of the array (left of the returned partition idx) and the right side (right of the returned partition idx), recursively
 */
+
+const nums1 = [11, 8, 14, 3, 6, 2, 7];
+const nums2 = [1, 17, 12, 3, 9, 13, 21, 4, 27];
+const { partitionHoare } = require("./partition");
+
+function quickSort(nums = [], left = 0, right = nums.length - 1) {
+  if (left < right) {
+    const pivotIndex = partitionHoare(nums, left, right);
+    console.log(pivotIndex);
+    quickSort(nums, left, pivotIndex - 1);
+    quickSort(nums, pivotIndex + 1, right);
+  }
+  return nums;
+}
