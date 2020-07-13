296e18bf344e76881857c4c300d7d95887fed608
Neil M <neilm813@gmail.com>
2020-07-13 09:54:33 -0700

    algo slsn

--- w2/d1/intersectSortedArraysDedupe.js
+++ w2/d1/intersectSortedArraysDedupe.js
@@ -5,7 +5,32 @@
   into an array containing the sorted set intersection of the two.
 
   Output: only the shared values between the two arrays (deduped).
-  
-  Input: [1,2,2,2,7], [2,2,6,6,7]
-  Output: [2,7]
 */
+
+const arrA = [0, 1, 2, 2, 2, 7];
+const arrB = [2, 2, 6, 6, 7];
+// Output: [2, 7]
+
+function intersectSortedArraysDedupe(arr1, arr2) {
+  let idx1 = 0,
+    idx2 = 0;
+
+  const len1 = arr1.length,
+    len2 = arr2.length,
+    result = [];
+
+  while (idx1 < len1 && idx2 < len2) {
+    if (arr1[idx1] === arr2[idx2]) {
+      if (result[result.length - 1] !== arr1[idx1]) {
+        result.push(arr1[idx1]);
+      }
+      idx1++;
+      idx2++;
+    } else if (arr1[idx1] < arr2[idx2]) {
+      idx1++;
+    } else {
+      idx2++;
+    }
+  }
+  return result;
+}
