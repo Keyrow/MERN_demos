7cb84dead81c654e365648323f41f3ccce461fb7
Neil M <neilm813@gmail.com>
2020-07-17 10:12:52 -0700

    algo slns

--- w2/d5/twoSum.js
+++ w2/d5/twoSum.js
@@ -9,7 +9,7 @@
 */
 const { testDriver } = require("../../helpers");
 
-const nums1 = [2, 7, 11, 15];
+const nums1 = [2, 11, 15, 7];
 const targetSum1 = 9;
 const expected1 = [0, 1];
 // Explanation: nums[0] + nums[1] = 2 + 7 = 9
@@ -17,4 +17,34 @@ const expected1 = [0, 1];
 const testCases = [{ arguments: [nums1, targetSum1], expected: expected1 }];
 testDriver([sumTwo, sumTwoSpaceOptimized], testCases);
 
-function sumTwo(nums, targetSum) {}
+// src: Morley Tatro
+// Time: O(n) linear, Space: O(n)
+function sumTwo(nums, targetSum) {
+  const numsAndIndices = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const diff = targetSum - num;
+
+    if (numsAndIndices.hasOwnProperty(diff)) {
+      // console.log(numsAndIndices);
+      return [numsAndIndices[diff], i];
+    }
+    numsAndIndices[num] = i;
+  }
+}
+
+// Time: O(n^2) quadratic, Space: O(1) constant
+function sumTwoSpaceOptimized(nums, targetSum) {
+  const indicies = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; ++j) {
+      if (nums[i] + nums[j] === targetSum) {
+        indicies.push(i, j);
+        break;
+      }
+    }
+  }
+  return indicies;
+}
